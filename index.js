eee6e9699061fbaed3a15bc0fa3955245b839cc3
Zhero-Six <vmochoge@gmail.com>
2025-04-02 20:01:02 +0300

    solved for index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+// index.js
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    
+    speak() {
+        if (this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+// Export classes for testing
+if (typeof module !== 'undefined') {
+    module.exports = { Cat, Dog, Bird };
+}
